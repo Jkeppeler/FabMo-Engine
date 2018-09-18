cdf0101d79bda3b2f195a972b2c2620bbbce9699
Ryan Sturmer <ryansturmer@gmail.com>
2018-09-18 19:06:12 +0000

    incorporate fmu unpack logic.  Update gitignore.

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-cd /tmp; \
-wget https://github.com/FabMo/FabMo-Engine/releases/download/v1.7.0/fabmo-consolidated-engine-1.7.0-updater-2.3.3.fmp; \
-/fabmo/updater/hooks/linux/edison/do_fmu.sh /tmp/fabmo-consolidated-engine-1.7.0-updater-2.3.3.fmp
+wget https://github.com/FabMo/FabMo-Engine/releases/download/v1.7.0/fabmo-consolidated-engine-1.7.0-updater-2.3.3.fmp -O /opt/fabmo/fabmo-update.fmp; 
 
+cd /tmp
+
+echo "Creating fmu directory..."
+rm -rf ./fmu
+mkdir ./fmu
+
+echo "Entering fmu directory..."
+cd ./fmu
+
+echo "Unpacking FMU: $1"
+gunzip < $1 | tar -xvf -
+rm -rf /opt/fabmo/fabmo-update.fmp
+
+echo "Applying update..."
+/bin/sh ./install
